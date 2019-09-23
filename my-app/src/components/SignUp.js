1445f72efe5b1588d0e4711177ce6f235c472402
MichaelBaynon <baynonmichael@gmail.com>
2019-09-23 14:46:51 -0400

    added signup

--- my-app/src/components/SignUp.js
+++ my-app/src/components/SignUp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import CV from "../imgs/collegeValue.png";
 
-const Login = props => {
+const SignUp = props => {
   return (
     <div className="loginPage">
       <Link className="titleLink" to="/">
@@ -12,24 +12,24 @@ const Login = props => {
           College Value
         </h1>
       </Link>
-      <h3 className="pageDesc">Log In</h3>
+      <h3 className="pageDesc">Sign Up</h3>
       <form className="loginForm">
         <h2>Username:</h2>
         <input type="text" name="username" placeholder="username" />
         <h2>Password:</h2>
         <input type="password" name="password" placeholder="password" />
-        <button type="submit">Log In</button>
+        <button type="submit">Sign Up</button>
       </form>
       <div>
-        <p> Don't have an account?</p>
+        <p> Already have an account?</p>
 
-        <Link className="loginSignUpLink" to="/signup">
+        <Link className="loginSignUpLink" to="/login">
           {" "}
-          Sign Up
+          Log In
         </Link>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default SignUp;
