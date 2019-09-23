9f240ba6a4545c080acbd84067a5e34a682b083a
MichaelBaynon <baynonmichael@gmail.com>
2019-09-23 14:36:42 -0400

    loggin setup and styling

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {Link} from 'react-router-dom'
+import CV from '../imgs/collegeValue.png'
 
 const Login = props => {
   return (
-  <>
-  <h1>Login Here</h1>
-    <form>
-      <input />
-    </form>
-  </>
-  )
+    <div className='loginPage'>
+        <Link className='titleLink' to='/'>
+      <h1 className="LoginTitle"><img src={CV} width='60' height='60' />College Value</h1>
+      </Link>
+      <form className="loginForm">
+        <h2>Username:</h2>
+        <input type="text" name="username" placeholder="username" />
+        <h2>Password:</h2>
+        <input type="password" name="password" placeholder="password" />
+        <button type="submit">Log In</button>
+      </form>
+      <div>
+          <p> Don't have an account?</p>
+      
+          <Link className='loginSignUpLink' to='/signup'> Sign Up</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
