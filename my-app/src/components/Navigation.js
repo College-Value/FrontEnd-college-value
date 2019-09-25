fc3c553142d551418321aaa19222d573d327e67f
MichaelBaynon <baynonmichael@gmail.com>
2019-09-25 12:58:23 -0400

    trying to clean

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,21 +1,18 @@
-  
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div>
       <div className="nav">
-     
-     
-        <div>
+        <div className='navLink'>
           <Link to="/">Home</Link>
         </div>
-        <div>
+        <div className='navLink'>
           <Link to="/about">About</Link>
         </div>
-        <div>
-          <Link to="">Predictions</Link>
+        <div className='navLink'>
+          <Link to="">Predictionssss</Link>
         </div>
       </div>
     </div>
