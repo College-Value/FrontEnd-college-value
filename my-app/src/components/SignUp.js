3e02e678b0b0f6972ecc59e8703d0315609ce731
MichaelBaynon <baynonmichael@gmail.com>
2019-09-25 17:51:03 -0400

    both login and signup working

--- my-app/src/components/SignUp.js
+++ my-app/src/components/SignUp.js
@@ -2,54 +2,59 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import CV from "../imgs/largelogo.png";
-import Navigation from './Navigation'
+import Navigation from "./Navigation";
 
 const SignUp = props => {
-const [user, setUser] = useState(null)
+  const [user, setUser] = useState({username: '', password: ''});
 
-const handleChange = e => {
-  e.preventDefault()
-  setUser({...user, [e.target.name]: e.target.value })
-}
+  const handleChange = e => {
+    e.preventDefault();
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-const handleSubmit = e => {
-  e.preventDefault()
-  axios
-  .post('https://gcj2-college-value.herokuapp.com/users/users',  `grant_type=password&username=${user.username}&password=${user.password}`, {
-    headers: {
-      Authorization: 'Basic bGFtYmRhLWNsaWVudDpsYW1iZGEtc2VjcmV0',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  })
-  .then(res => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(user.username)
+    console.log(user.password)
+    axios
+      .post(
+        "https://gcj2-college-value.herokuapp.com/createnewuser", user)
+      .then(res => {
+        props.history.push("/login");
       
-      props.history.push('/login')
-      
-      
-      
-  })
-  .catch( err => console.log(err.response))
-}
+      })
+      .catch(err => console.log(err.response));
+  };
 
   return (
     <div className="loginPage">
       <Navigation />
       <Link className="titleLink" to="/">
         <h1 className="LoginTitle">
-          <img src={CV} width="60" height="60" alt="college value logo"/>
+          <img src={CV} width="60" height="60" alt="college value logo" />
           College Value
         </h1>
       </Link>
       <p className="pageDesc">Sign Up</p>
-      <form   onSubmit={handleSubmit} className="loginForm">
+      <form onSubmit={handleSubmit} className="loginForm">
         <h2>Username:</h2>
-        <input type="text" name="username" placeholder="username"  onChange={handleChange}/>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleChange}
+        />
         <h2>Password:</h2>
-        <input type="password" name="password" placeholder="password"  onChange={handleChange}/>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
         <button type="submit">Sign Up</button>
       </form>
       <div>
-        <p className='question'> Already have an account?</p>
+        <p className="question"> Already have an account?</p>
 
         <Link className="loginSignUpLink" to="/login">
           {" "}
